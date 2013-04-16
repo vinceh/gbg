2000a7fb5fb9a11960dd5d10665876a8f719f7fc
Vincent Chu <c.tnecniv@gmail.com>
2013-04-15 22:36:58 -0700

    search bar loader

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -4,11 +4,29 @@ $(window).load(function() {
     });
 
     $('.search-form').submit(function() {
+
+        var searchinput = $('.search-input');
+        var errortext = $('.formtext');
+        var ball = $(this).children('img');
+
+        if (searchinput.val() == "") {
+            errortext.show();
+            return false;
+        }
+
+        errortext.hide();
+        ball.show();
         $.ajax({
-            url: '/single_search?term='+$('.search-input').val()
+            url: '/single_search?term='+searchinput.val(),
+            complete: function() {
+                ball.hide();
+            }
         }).done( function(data) {
             if ( data.id ) {
                 window.location = "/boardgame/"+data.id;
+            }
+            else {
+                errortext.show();
             }
         });
 
